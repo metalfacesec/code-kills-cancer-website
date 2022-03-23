f3e5dda5ab9bf4ba6d073f0ce801702fd4daec06
Metal Face <50063043+metalfacesec@users.noreply.github.com>
2022-03-23 19:15:46 -0400

    Pushing up template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import TextEditor from './components/shared/TextEditor';
+
+function App() {
+	return (
+		<div className='app-container'>	
+			<TextEditor />
+		</div>
+	);
+}
+
+export default App;
